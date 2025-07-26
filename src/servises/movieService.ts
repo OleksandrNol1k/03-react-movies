9d37b14c7cbb11936d45161c76525b11dabe98bb
Oleksandr Nol1k <nevskij.mail@gmail.com>
2025-07-27 02:39:33 +0300

    change URL & .env

--- src/servises/movieService.ts
+++ src/servises/movieService.ts
@@ -1,8 +1,6 @@
 import axios from "axios"
 import type { Movie } from "../types/movie"
 
-axios.defaults.baseURL = "https://api.themoviedb.org/3";
-
 interface FetchMoviesResp {
   hits: Movie[];
 }
@@ -11,7 +9,7 @@ export const fetchMovies = async (query: string): Promise<Movie[]> => {
     
     const myKey = import.meta.env.VITE_TMDB_TOKEN;
   
-    const response = await axios.get<FetchMoviesResp>(`/movie?query=${query}`, {
+    const response = await axios.get<FetchMoviesResp>(`https://api.themoviedb.org/3/search/movie?query=${query}`, {
         params: {},
         headers: {
             Authorization: `Bearer ${myKey}`,
